3afb578b8ffb4cae6e889c3f113ab304717ec05c
willop9 <willop9@googlemail.com>
2019-01-26 00:51:29 +0000

    Pythoin challenge that sorts array in a really dumb way

---
+++ pythonChallenge.py
@@ -0,0 +1,17 @@
+#Python challenge to sort a numer array from smallest to largest
+import random
+
+numbers = []
+for i in range(10):
+    x = random.randint(1,101)
+    numbers.append(x)
+    print(x)
+print(numbers)
+
+for i in range (len(numbers)):
+    for j in range (len(numbers)):
+        if numbers[i] < numbers[j]:
+            temp = numbers[j]
+            numbers[j] = numbers[i]
+            numbers[i] = temp
+print(numbers)
