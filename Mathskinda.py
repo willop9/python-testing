2c6c72720b567e864669ba3f59e50f1bb80aa779
willop9 <willop9@googlemail.com>
2019-01-26 14:35:17 +0000

    basic multiplication and print function diff data types

--- Mathskinda.py
+++ Mathskinda.py
@@ -1,2 +1,8 @@
 from math import *
-print(floor(sqrt(49)));
+print(floor(sqrt(49)))
+
+i = input('Enter number 1: ')
+j = input('Enter number 2: ')
+result = float(i)*float(j)
+
+print(str(i) + ' multiplied by ' + str(j) + ' is ' + str(result))
