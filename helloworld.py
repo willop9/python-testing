398d6df42f970788a0d7df3efd4fe4315de60a3b
William <willop9@googlemail.com>
2019-01-29 00:31:30 +0000

    Created point class and created two point objects

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,14 @@
 print("hello World, this is my first python program")
 name = "Will"
 print("hello " + name)
+
+class Point:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+p = Point()
+q = Point()
+
+print(p.x, p.y, q.x, q.y)
