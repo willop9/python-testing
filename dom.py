48da7138de0819287f1af3f043b8b3b8e66f3f07
William <willop9@googlemail.com>
2019-02-27 16:12:15 +0000

    Changes at the library. Working out beautiful soup

--- dom.py
+++ dom.py
@@ -1,10 +1,23 @@
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
-mysite = urllib.request.urlopen('http://www.google.com').read()
-soup = BeautifulSoup(mysite)
+mysite = urllib.request.urlopen('http://www.bbc.co.uk').read()
+soup = BeautifulSoup(mysite, 'html5lib')
 
-description = soup.find("meta", {"name": "description"}) # meta tag description
-text = description['content'] # text of content attribute
+#description = soup.find("meta", {"name": "description"}) # meta tag description
+#print(soup.prettify)
+site2 = urllib.request.urlopen("file:./docs/page.html")
+soup2 = BeautifulSoup(site2, 'lxml')
+#print(soup2.get_text())
+#print(soup2.prettify())
+head_tag = soup2.head
+#for child in head_tag.descendants:
+ #   print(child)
 
-print(text)
+print(len(soup2.contents))
+print(soup2.contents[len(soup2.contents) -1].name)
+
+print(len(head_tag.contents))
+
+for child in head_tag.contents:
+    print(head_tag.contents[child].name)
